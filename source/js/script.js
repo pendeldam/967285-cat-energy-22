3906e7c6404efc889116e3e0353c4fe7f7ce073a
Алексей Сергеев <pendelladam@gmail.com>
2021-05-11 00:11:24 +0300

    WIP: prepared build

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,20 @@
+const mainNav = document.querySelector(`.main-nav`);
+const mainNavToggle = mainNav.querySelector(`.main-nav__toggle`);
+
+mainNav.classList.remove(`no-js`);
+
+mainNavToggle.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+
+  if (mainNav.classList.contains(`main-nav--opened`)) {
+    mainNav.classList.remove(`main-nav--opened`);
+    mainNav.classList.add(`main-nav--closed`);
+  } else {
+    mainNav.classList.remove(`main-nav--closed`);
+    mainNav.classList.add(`main-nav--opened`);
+  }
+});
+
 const slider = document.querySelector(`.slider`);
 const slideAfter = slider.querySelector(`.slider__image-after`);
 const sliderButtons = slider.querySelectorAll(`.slider__button`);
